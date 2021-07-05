1f00117607d944dda3f4ab1566a07ca0d1467a20
caijianying <caijianying@shulie.io>
2021-07-05 11:12:48 +0800

    flyway开始

---
+++ flyway-demo/src/main/java/com/example/flywaydemo/FlywayDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.flywaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author caijianying
+ */
+@SpringBootApplication
+public class FlywayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayDemoApplication.class, args);
+    }
+
+}
---
+++ flyway-demo/src/test/java/com/example/flywaydemo/FlywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flywaydemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
