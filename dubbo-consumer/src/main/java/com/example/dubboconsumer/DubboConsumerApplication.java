c3bd798ce1b8a7821c261ebdd853a352940d84f5
caijy <956203814@qq.com>
2021-07-12 20:58:01 +0800

    maven依赖管理

--- dubbo-consumer/src/main/java/com/example/dubboconsumer/DubboConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/example/dubboconsumer/DubboConsumerApplication.java
@@ -3,12 +3,13 @@ package com.example.dubboconsumer;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author caijianying
  */
 @EnableDubbo
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
